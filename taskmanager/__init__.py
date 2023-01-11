f0eac01745c951746e749be7637ad2724b0613ad
alcl2000 <amy.555.lewis@gmail.com>
2023-01-11 10:57:47 +0000

    fix: env.py correctly installed

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env.py
+    import env
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
