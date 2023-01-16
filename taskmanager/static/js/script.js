a5d1204a6496855b30591d2cbc24eaa92949be50
alcl2000 <amy.555.lewis@gmail.com>
2023-01-16 15:45:53 +0000

    add: datepicker and select form element func

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.modal').modal();
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(datepicker,{
+    format: "dd mmm yyyy",
+    i18n: {done: "select"}
+  });
+});
+$(document).ready(function(){
+  $('select').formSelect();
+});
+      
