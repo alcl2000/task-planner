35f2299a089ca76ecfcce88494c1674080f9fba7
alcl2000 <amy.555.lewis@gmail.com>
2023-01-16 16:28:35 +0000

    fix: due_date input fix

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,13 +4,11 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.modal').modal();
 });
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.datepicker');
-  var instances = M.Datepicker.init(datepicker,{
-    format: "dd mmm yyyy",
-    i18n: {done: "select"}
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
-});
 $(document).ready(function(){
   $('select').formSelect();
 });
